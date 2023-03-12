312a93e1220f2b7c9b087f5ac958e4e3e302d6a3
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-12 10:55:52 +0530

    Constructor1

---
+++ constructor.java
@@ -0,0 +1,28 @@
+// constructor
+// normal flow of the constructor
+package com.bridgelabz.oops;
+
+public class constructor {
+void m1(){
+    System.out.println("m1 method");
+}
+
+    constructor(){
+        System.out.println("0 arg constructor");
+    }
+    constructor(int a){
+        System.out.println("1 arg constructor");
+    }
+    constructor(int a, int b){
+        System.out.println("2 arg constructor");
+    }
+
+    public static void main(String[] args) {
+        constructor c = new constructor();
+        new constructor(10);
+        new constructor(10,20);
+        c.m1();
+    }
+
+
+}
