4eabf4242dafa25c61ec90d80c65a75454ffdf0c
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-14 14:10:03 +0530

    poly3 added

---
+++ PolyMor3.java
@@ -0,0 +1,13 @@
+// operator overloading
+// one operator with one or more behaviour is called operator overloading
+// only one opertor supports in java (+)
+// java is not suppport operator oveloading
+package com.bridgelabz.oops;
+
+public class PolyMor3 {
+    public static void main(String[] args) {
+        System.out.println(10+20);
+        System.out.println("prajwal"+"gunjal");
+// here plus operator can add integer also and doing concatenation of two string so one operator but diff behaviour
+    }
+}
