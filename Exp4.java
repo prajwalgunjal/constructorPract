59e704b122442a8fa3fecf894d61e6533b671554
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-03 23:14:41 +0530

    Exp4 Added

---
+++ Exp4.java
@@ -0,0 +1,12 @@
+// if you are writing try block then you have to write catch block
+package com.bridgelabz.oops;
+public class Exp4 {
+    public static void main(String[] args) {
+        try{
+            System.out.println("Try block");
+        }
+    }
+}
+
+
+//op :- java: 'try' without 'catch', 'finally' or resource declarations
