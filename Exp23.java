68cb9e7f7d30c742c935f96897f66b64cd941c7a
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-04-04 22:58:36 +0530

    Exp 23 Added

---
+++ Exp23.java
@@ -0,0 +1,11 @@
+// you can write try and finally without catch
+package com.bridgelabz.oops;
+public class Exp23 {
+    public static void main(String[] args) {
+        try{
+            System.out.println("try block");
+        }finally {
+            System.out.println("Finally block");
+        }
+    }
+}
